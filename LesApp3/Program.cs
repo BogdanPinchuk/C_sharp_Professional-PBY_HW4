1e8d35315d9f3f915e3e9108f81d474ce3932f24
Bogdan <pinchuk.brus@gmail.com>
2019-09-26 14:28:11 +0300

    Finish Apps

--- LesApp3/Program.cs
+++ LesApp3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -19,13 +20,73 @@ namespace LesApp3
             // join unicode
             Console.OutputEncoding = Encoding.Unicode;
 
-            string data = "# IFresh fresh 0,50 л 1 шт. x 7,99₴ = 4,00₴";
-            Console.WriteLine(data);
+            // адрес файла
+            string path = "CheckFile";
 
-            Console.WriteLine(Regex.Match(data, @"# [ \w]+ ").Value.Trim('#').Trim(' '));
+            // Оновлюємо курс валют згідно НБУ - з офіційного сайту
+            NBU.Update();
+            // Виводимо курс валют
+            Show(NBU.ToString());
+            Console.WriteLine("\n" + new string('#', 80) + "\n");
+
+            // Створення "текстового чеку" - електроного чека і збереження його в файл
+            Check check = new Check();
+
+            #region Занесення даних
+            check.Products.Add(new Position()
+            {
+                Name = "IFresh",
+                Count = 1,
+                Price = 7.99,
+                Volume = 0.5,
+            });
+            check.Products.Add(new Position()
+            {
+                Name = "Meat",
+                Count = 1,
+                Price = 225.00,
+                Weigth = 0.575,
+            });
+            check.Products.Add(new Position()
+            {
+                Name = "Bread white",
+                Count = 1,
+                Price = 15.00,
+            }); 
+            #endregion
+
+            // збереження
+            check.SaveCheckFile(path);
+
+            // показуємо файл
+            //Process.Start(path + ".txt");
+            //Console.ReadKey(true);
+
+            // створення нового електронного чека з одночасним розпізнаванням
+            Check newCheck = new Check(path);
+
+            // виведення укр версії в консоль
+            Show("\tУкраїнська версія (в гривнях):\n");
+            Console.WriteLine(newCheck.ToString());
+
+            // виведення англ версії в консоль
+            newCheck.SetDolar();
+            Show("\tАмериканська версія (в доларах):\n");
+            Console.WriteLine(newCheck.ToString());
 
             // delay
             Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Показ повідомлення в зеленому кольорі
+        /// </summary>
+        /// <param name="s">Повідомлення</param>
+        private static void Show(string s)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(s);
+            Console.ResetColor();
         }
     }
 }
